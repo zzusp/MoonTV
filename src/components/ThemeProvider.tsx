d9fa4866dcf5f8f594050dc501a5f087a5db50bb
shinya <kobayashi_shinya@outlook.com>
2025-06-26 21:07:10 +0800

    fix: theme provider

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,9 +1,18 @@
 'use client';
 
+import type { ThemeProviderProps } from 'next-themes';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import type { ThemeProviderProps } from 'next-themes/dist/types';
 import * as React from 'react';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider
+      attribute='class'
+      defaultTheme='system'
+      enableSystem
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
