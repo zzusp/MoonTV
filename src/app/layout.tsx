8afbc0e0afa786e286ec3649f7f5bb2c583dd302
shinya <kobayashi_shinya@outlook.com>
2025-07-14 10:13:32 +0800

    fix: fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const inter = Inter({ subsets: ['latin'] });
 // 动态生成 metadata，支持配置更新后的标题变化
 export async function generateMetadata(): Promise<Metadata> {
   let siteName = process.env.NEXT_PUBLIC_SITE_NAME;
-  if (process.env.NEXT_PUBLIC_STORAGE_TYPE === 'd1') {
+  if (process.env.NEXT_PUBLIC_STORAGE_TYPE !== 'd1') {
     const config = await getConfig();
     siteName = config.SiteConfig.SiteName;
   }
