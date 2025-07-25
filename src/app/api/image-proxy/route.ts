1be22a23980e5d5fdcc0ebad6c5034e1bedd7c6b
shinya <kobayashi_shinya@outlook.com>
2025-07-25 21:13:35 +0800

    feat: vercel cdn cache

--- src/app/api/image-proxy/route.ts
+++ src/app/api/image-proxy/route.ts
@@ -43,7 +43,9 @@ export async function GET(request: Request) {
     }
 
     // 设置缓存头（可选）
-    headers.set('Cache-Control', 'public, max-age=15720000'); // 缓存半年
+    headers.set('Cache-Control', 'public, max-age=15720000, s-maxage=15720000'); // 缓存半年
+    headers.set('CDN-Cache-Control', 'public, s-maxage=15720000');
+    headers.set('Vercel-CDN-Cache-Control', 'public, s-maxage=15720000');
 
     // 直接返回图片流
     return new Response(imageResponse.body, {
