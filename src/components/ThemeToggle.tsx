8972e1697a871ac54a3b6381a9304fff2aa019ca
shinya <kobayashi_shinya@outlook.com>
2025-07-29 18:48:22 +0800

    feat: revert background color

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -20,11 +20,6 @@ export function ThemeToggle() {
     } else {
       meta.setAttribute('content', theme === 'dark' ? '#0c111c' : '#f9fbfe');
     }
-
-    document.documentElement.style.backgroundColor =
-      theme === 'dark' ? '#0c111c' : '#f9fbfe';
-    document.body.style.backgroundColor =
-      theme === 'dark' ? '#0c111c' : '#f9fbfe';
   };
 
   useEffect(() => {
